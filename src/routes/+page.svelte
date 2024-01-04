884be0d8de28969886429c61af7db539c16a896d
Yuki Takase <henoya@gmail.com>
2024-01-04 20:40:47 +0900

    tailwindcss を追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="bg-black">
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+    <p class="text-xl text-white">「tailwindは有効です」</p>
+</main>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
