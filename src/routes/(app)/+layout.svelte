b5b6dea1cd8c541cdeeceb91280be4b405bb6223
Yuki Takase <henoya@gmail.com>
2024-01-06 10:45:19 +0900

    routes/(app) 以下にページ等を移動・import aliasを定義

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.css';
+	import '$src/app.pcss';
 </script>
 
 <svelte:head>
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		// importで参照するディレクトリのエイリアス設定
+		alias: {
+			$src: 'src',
+			'$src/*': 'src',
+			$features: 'src/features',
+			'$features/*': 'src/features',
+			$routes: 'src/routes',
+			'$routes/*': 'src/routes'
+		}
 	}
 };
 
