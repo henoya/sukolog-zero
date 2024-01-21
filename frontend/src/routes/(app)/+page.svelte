87f3e1daa3cd25b00283c6fe4fa919dae59776d3
henoya <henoya@gmail.com>
2024-01-21 23:11:16 +0900

    ひとまずコンポーネント配置

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,26 +1,31 @@
-<script lang="ts">
-  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte';
+<script lang='ts'>
+	import {
+		Avatar,
+		Button,
+		Dropdown,
+		DropdownDivider,
+		DropdownHeader,
+		DropdownItem,
+		ImagePlaceholder,
+		Navbar,
+		NavBrand,
+		NavHamburger,
+		NavLi,
+		NavUl,
+		Skeleton,
+		TextPlaceholder
+	} from 'flowbite-svelte';
 </script>
 
-<Navbar>
-  <NavBrand href="/">
-    <img src="/images/favicon.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
-  </NavBrand>
-  <NavUl>
-    <NavLi href="/" active={true}>Home</NavLi>
-    <NavLi href="/test">Test</NavLi>
-    <NavLi href="/about">About</NavLi>
-  </NavUl>
-</Navbar>
 <div class='relative px-8'>
 	<Navbar class='px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b'>
 		<NavBrand href='/'>
-			<!--      <img src="/images/sukolog-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />-->
-			<span class='self-center whitespace-nowrap text-xl font-bold dark:text-white'>Sukolog-ZERO</span>
+			<img src='/favicon.png' class='me-3 h-6 sm:h-9' alt='Flowbite Logo' />
+			<span class='self-center whitespace-nowrap text-xl font-bold dark:text-white'>Sukolog-ZERO</span
+			>
 		</NavBrand>
-		<div class='flex items-center md:order-2'>
-			<!--			<Button size='sm'>Login</Button>-->
+		<div class='flex left-auto md:order-2'>
+			<Button size='sm'>Login</Button>
 			<Avatar id='avatar-menu' src='/favicon.png' />
 			<NavHamburger class1='w-full md:flex md:w-auto md:order-1' />
 		</div>
@@ -29,15 +34,21 @@
 				<span class='block text-sm'>Bonnie Green</span>
 				<span class='block truncate text-sm font-medium'>name@flowbite.com</span>
 			</DropdownHeader>
-<!--			<DropdownItem>Dashboard</DropdownItem>-->
-<!--			<DropdownItem>Settings</DropdownItem>-->
-<!--			<DropdownItem>Earnings</DropdownItem>-->
-<!--			<DropdownDivider />-->
-<!--			<DropdownItem>Sign out</DropdownItem>-->
+			<DropdownItem>Dashboard</DropdownItem>
+			<DropdownItem>Settings</DropdownItem>
+			<DropdownItem>Earnings</DropdownItem>
+			<DropdownDivider />
+			<DropdownItem>Sign out</DropdownItem>
 		</Dropdown>
-		<NavUl>
+		<NavUl class='flex left-auto md:order-1'>
 			<NavLi href='/' active={true}>Home</NavLi>
+			<NavLi href='/test'>Test</NavLi>
 			<NavLi href='/about'>About</NavLi>
 		</NavUl>
 	</Navbar>
+	<div>
+		<Skeleton class='mt-16 mb-8' />
+		<ImagePlaceholder class='my-8' />
+		<TextPlaceholder class='my-8' />
+	</div>
 </div>
--- frontend/src/routes/(app)/test/+page.svelte
+++ frontend/src/routes/(app)/test/+page.svelte
@@ -1,48 +1,47 @@
 <script lang="ts">
-  import { Button } from 'flowbite-svelte';
-  import { Dropdown, DropdownItem } from '$lib/flowbite-svelte-patch';
+  import { Button, Dropdown, DropdownItem, DropdownDivider } from 'flowbite-svelte';
   import { ChevronDownSolid, ChevronRightSolid } from 'flowbite-svelte-icons';
 import type { Placement } from '@floating-ui/dom';
   let placement: Placement;
       placement = "right-start";
   placement = "right-start"
   console.log(placement)
-  // import { page } from '$app/stores';
-  // $: activeUrl = $page.url.pathname;
-  // let activeClass = 'text-blue-700 dark:text-blue-300 hover:text-blue-900 dark:hover:text-blue-500';
+  import { page } from '$app/stores';
+  $: activeUrl = $page.url.pathname;
+  let activeClass = 'text-blue-700 dark:text-blue-300 hover:text-blue-900 dark:hover:text-blue-500';
 </script>
 
-<!--<Button>Dropdown button<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>-->
-<!--<Dropdown>-->
-<!--  <div slot="header" class="px-4 py-2">-->
-<!--    <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>-->
-<!--    <span class="block truncate text-sm font-medium">name@flowbite.com</span>-->
-<!--  </div>-->
-<!--  <DropdownItem>Dashboard</DropdownItem>-->
-<!--  <DropdownDivider />-->
-<!--  <DropdownItem>Settings</DropdownItem>-->
-<!--  <DropdownItem>Earnings</DropdownItem>-->
-<!--  <DropdownItem slot="footer">Separated link</DropdownItem>-->
-<!--</Dropdown>-->
+<Button>Dropdown button<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>
+<Dropdown>
+ <div slot="header" class="px-4 py-2">
+   <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+   <span class="block truncate text-sm font-medium">name@flowbite.com</span>
+ </div>
+ <DropdownItem>Dashboard</DropdownItem>
+ <DropdownDivider />
+ <DropdownItem>Settings</DropdownItem>
+ <DropdownItem>Earnings</DropdownItem>
+ <DropdownItem slot="footer">Separated link</DropdownItem>
+</Dropdown>
 
-<!--<Button>Dropdown button<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>-->
-<!--<Dropdown>-->
-<!--  <div slot="header" class="px-4 py-2">-->
-<!--    <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>-->
-<!--    <span class="block truncate text-sm font-medium">name@flowbite.com</span>-->
-<!--  </div>-->
-<!--  <DropdownItem>Dashboard</DropdownItem>-->
-<!--  <DropdownItem>Settings</DropdownItem>-->
-<!--  <DropdownItem>Earnings</DropdownItem>-->
-<!--  <DropdownItem slot="footer">Sign out</DropdownItem>-->
-<!--</Dropdown>-->
+<Button>Dropdown button<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>
+<Dropdown>
+ <div slot="header" class="px-4 py-2">
+   <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+   <span class="block truncate text-sm font-medium">name@flowbite.com</span>
+ </div>
+ <DropdownItem>Dashboard</DropdownItem>
+ <DropdownItem>Settings</DropdownItem>
+ <DropdownItem>Earnings</DropdownItem>
+ <DropdownItem slot="footer">Sign out</DropdownItem>
+</Dropdown>
 
-<!--<Button>Dropdown button<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>-->
-<!--<Dropdown {activeUrl} {activeClass}>-->
-<!--  <DropdownItem href="/">Home</DropdownItem>-->
-<!--  <DropdownItem href="/test">Test</DropdownItem>-->
-<!--  <DropdownItem href="/about">About</DropdownItem>-->
-<!--</Dropdown>-->
+<Button>Dropdown button<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>
+<Dropdown {activeUrl} {activeClass}>
+ <DropdownItem href="/">Home</DropdownItem>
+ <DropdownItem href="/test">Test</DropdownItem>
+ <DropdownItem href="/about">About</DropdownItem>
+</Dropdown>
 
 <Button>Dropdown button<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>
 <Dropdown>
